13cb129aa5a6076b202acfe67bd28a8d94d6fd4b
Kay Zou <zouzhuoy@shernet.sheridancollege.ca>
2025-01-16 10:17:18 -0500

    humm

--- Assets/script/solar.cs
+++ Assets/script/solar.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class solar : MonoBehaviour
 {
     public float speed = 5;
+    public float showingup = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,6 @@ public class solar : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, 0, speed * Time.deltaTime);
+        
     }
 }
