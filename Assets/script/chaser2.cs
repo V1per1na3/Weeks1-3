9df95f3cd599a98d397a5c235485944944832951
kayval135@gmail.com <kayval135@gmail.com>
2025-01-20 14:52:15 -0500

    game object run away from the mouse (test distance)

---
+++ Assets/script/chaser2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaser2 : MonoBehaviour
+
+{
+    Vector2 mousepos;
+    float dir = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        Vector2 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (Vector2.Distance(mousepos,pos)<= dir)
+        {
+            transform.position = new Vector2(Random.Range(-9, 10), Random.Range(-4, 4));
+
+        }
+
+    }
+}
--- Assets/script/chaserpos.cs
+++ Assets/script/chaserpos.cs
@@ -21,6 +21,7 @@ public class chaserpos : MonoBehaviour
 
             transform.position = mousepos;
 
-        }  
+        }
+        
     }
 }
