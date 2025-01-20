2906ffd1189782388d5e381269a26bb8d5dacc38
kayval135@gmail.com <kayval135@gmail.com>
2025-01-20 13:17:52 -0500

    chaser move to position where mouse is clicked

---
+++ Assets/script/chaserpos.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaserpos : MonoBehaviour
+{
+    Vector2 mousepos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position = mousepos;
+
+        }  
+    }
+}
