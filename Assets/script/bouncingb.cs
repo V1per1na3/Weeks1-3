1f6f64d3e518afd1c213ba188492b92789b139fb
kayval135@gmail.com <kayval135@gmail.com>
2025-01-19 21:07:01 -0500

    add in keyboard control (speed, size, reset position) for bouncing ball

--- Assets/script/bouncingb.cs
+++ Assets/script/bouncingb.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 
 public class bouncingb : MonoBehaviour
 {
-
-    public float bSpeed = 0;//speed
+    public float runspeed = 1.5f;//x speed
+    public float bSpeed = 0;//y speed
     public float grav = 0.01f;//gravity
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +18,9 @@ public class bouncingb : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
-
+        Vector2 bsize = transform.localScale;
         pos.y -= Time.deltaTime * bSpeed;//move in y axis
-        pos.x += Time.deltaTime* 1.5f;//move in x axis
+        pos.x += Time.deltaTime* runspeed;//move in x axis
         bSpeed += grav;//update speed
         Vector2 ballinscreenspace = Camera.main.WorldToScreenPoint(pos);
         if (ballinscreenspace.y <= 0)//check if ball hits the ground
@@ -33,9 +32,32 @@ public class bouncingb : MonoBehaviour
         {
             pos.x = -10;
         }
+        
+        if (Input.GetKeyDown(KeyCode.Space))//reset ball position
+        {
+            pos = Vector2.zero;
+         
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            runspeed += 0.2f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            runspeed -= 0.2f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            bsize.x +=0.5f;
+            bsize.y += 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            bsize.x -= 0.5f;
+            bsize.y -= 0.5f;
+        }
+        transform.localScale = bsize;
         transform.position = pos;
-
-
 
     }
 }
